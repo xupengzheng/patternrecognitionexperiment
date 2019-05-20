164e556debb93b556d9ed9173550403a5832d843
Eric <543709852@qq.com>
2019-05-20 15:21:19 +0800

    comment and readme edited

--- Cmeancluster.m
+++ Cmeancluster.m
@@ -1,7 +1,7 @@
 function [Mconver,Xlabelcover,niter,Xlabelhistory] = Cmeancluster(X,M)
 %   C均值聚类方法
 %   X为样本数据，M为初值中心 
-%   Mconver为最终收敛到的类别中心,Mhistory包含类别中心的每次迭代的历史结果
+%   Mconver为最终收敛到的类别中心,niter为迭代次数
 %   Xlabelcover为最终类别结果,Xlabelhistory每次迭代的类别历史结果
 nx = size(X,1);
 nclass = size(M,1);
