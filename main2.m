bbc0fae0f98f4208855aa879ee808fcab9020367
Eric <543709852@qq.com>
2019-05-20 21:37:21 +0800

    handed version

--- main2.m
+++ main2.m
@@ -5,10 +5,10 @@ rng(5);
 mu1 = [-2,-2];sigma1=[1 0;0 1];
 mu2 = [ 2,2]; sigma2=[1 0;0 4]; 
 %% 产生仿真数据
-N = 1100;
-X1 = mvnrnd(mu1,sigma1,1100);
-X2 = mvnrnd(mu2,sigma2,1100);
-Ntrain = 100;
+N = 10050;
+X1 = mvnrnd(mu1,sigma1,N);
+X2 = mvnrnd(mu2,sigma2,N);
+Ntrain = 50;
 X1Train = X1(1:Ntrain,:);
 X2Train = X2(1:Ntrain,:);
 XTrain = [X1Train,ones(Ntrain,1);X2Train,2*ones(Ntrain,1)];
